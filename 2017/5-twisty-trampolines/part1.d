1761360e954c83c7329c5fa9b89560d6741af587
Keith Wade <keithwade@nanonation.net>
2017-12-05 08:49:02 -0600

    Day 5 Part 1 complete

---
+++ 2017/5-twisty-trampolines/part1.d
@@ -0,0 +1,32 @@
+import std.stdio;
+import std.file;
+import std.conv;
+import std.algorithm;
+import std.string;
+import std.array;
+import std.math;
+
+void main()
+{
+  // Pull in input and prep for iteration
+  string input = readText("./input.txt");
+  string[] instructionStrings = input.splitLines();
+  int[] instructions = instructionStrings.map!(n => n.parse!int).array();
+
+  int steps = 0;
+
+  int pos = 0;
+  int max = to!int(instructions.length);
+
+  while (true)
+  {
+    int nextPos = pos + instructions[pos];
+    instructions[pos]++;
+    pos = nextPos;
+    steps++;
+    if (pos >= max) {
+      break;
+    }
+  }
+  writeln(steps);
+}
