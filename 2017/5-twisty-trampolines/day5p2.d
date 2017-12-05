e7cf947f4c9b53478f15b43ad6c435d2b38faf07
Keith Wade <keithwade@nanonation.net>
2017-12-05 09:29:04 -0600

    Update input

--- 2017/5-twisty-trampolines/day5p2.d
+++ 2017/5-twisty-trampolines/day5p2.d
@@ -8,26 +8,24 @@ import std.array;
 void main()
 {
   // Pull in input and prep for iteration
-  string input = readText("./input.txt");
-  string[] instructionStrings = input.splitLines();
-  int[] instructions = instructionStrings.map!(n => n.parse!int).array();
+  int[] input = readText("./input.txt").splitLines().map!(n => n.parse!int).array();
 
   int steps = 0;
 
   int pos = 0;
-  int max = to!int(instructions.length);
+  int max = to!int(input.length);
 
   while (!(pos >= max || pos < 0))
   {
-    int nextPos = pos + instructions[pos];
+    int nextPos = pos + input[pos];
 
-    if (instructions[pos] >= 3)
+    if (input[pos] >= 3)
     {
-      instructions[pos]--;
+      input[pos]--;
     }
     else
     {
-      instructions[pos]++;
+      input[pos]++;
     }
 
     pos = nextPos;
